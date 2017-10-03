e9eb9d1aab15fee601d7dec2923622ca3fe093ee
ilhamhanami <hanamiilham@gmail.com>
2017-10-03 20:54:51 +0700

    Update LinearList.java

--- src/algodatkelas_3d/LinearList.java
+++ src/algodatkelas_3d/LinearList.java
@@ -8,6 +8,8 @@ public interface LinearList {
     public void add(int index,Object obj);
     public String toString();
     public void changeLength2D(int index,Object obj);
-    
+    public Object clear();
+    public void removeRange(int fromIndex,int toIndex);
+    public Object clone(Object[]a);
     
 }
