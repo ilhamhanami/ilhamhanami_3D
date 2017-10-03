11a957cc32f57a5ab6485f1dc7d6a23303f67e57
ilhamhanami <hanamiilham@gmail.com>
2017-10-03 20:55:46 +0700

    Update Driver.java

--- src/algodatkelas_3d/Driver.java
+++ src/algodatkelas_3d/Driver.java
@@ -11,6 +11,8 @@ public class Driver {
         x.add(0, new Integer(2));
         x.add(0, new Integer(1));
         x.add(0, new Integer(0));
+        
+        Object[]a=new Object[15];
 
         System.out.println("banyak index/data = " + x.size);
         System.out.println("banyaknya kapasitas =" + x.capacity);
@@ -39,10 +41,25 @@ public class Driver {
         x.add(5,6);
         System.out.println(x.size);
         System.out.println("isi stlh d tambah 3 array baru : "+ x.toString());
-        System.out.println("jumlah array sebelum di cut :" + x.element.length);
+        System.out.println("jumlah array sebelum dipotong :" + x.element.length);
         x.trimToSize();
+        x.setSize(7);
         System.out.println("jumlah array setelah dipotong :" + x.element.length);
         x.setSize(6);
         System.out.println("isi array setelah di setsize"+x.toString());
+        System.out.println("remove range");
+        x.removeRange(1, 2);
+        System.out.println("panjang array :"+x.element.length);
+        System.out.println("array setelah remove ="+x.toString());
+        
+        System.out.println();
+        System.out.println("panjang array= "+x.size);
+        System.out.println("panjang element= "+x.element.length);
+        System.out.println("data dipanggil melalui X ="+x.toString());
+        System.out.println("data dipanggil melalu Y="+x.clone(a));
+        
+        System.out.println("");
+        x.clear();
+        System.out.println("array setelah di clear= "+x.toString());
     }
 }
